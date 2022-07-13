41be6783fc2483f3d6e95edad00ecf8377670ec4
godzilli <jenniferdacostazilli@hotmail.com>
2022-07-12 22:41:19 -0300

    alura react

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,39 @@
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from './cronometro.module.scss';
+import { tempoParaSeg } from "../../common/utils/date";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+
+interface Props {
+    selecionado: ITarefa | undefined,
+    finalizar: () => void
+}
+
+export function Cronometro ({ selecionado, finalizar} : Props ){
+    const [ tempo, setTempo ] = useState<number>();
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSeg(selecionado.tempo));
+        }
+    }, [selecionado])
+
+    function regressiva(contador: number = 0){
+        setTimeout(() => {
+            if(contador > 0){
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+            finalizar();
+        }, 1000);
+    }
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
+            <div className={style.relogioWrapper}>
+                <Relogio tempo={tempo}/>
+            <Botao onClick={() => regressiva(tempo)}> Começar! </Botao>
+            </div>
+        </div>
+    )
+}
---
+++ src/types/tarefa.ts
@@ -0,0 +1,8 @@
+
+export interface ITarefa{
+    tarefa: string,
+    tempo: string,
+    completado: boolean,
+    selecionado: boolean,
+    id: string
+  }
